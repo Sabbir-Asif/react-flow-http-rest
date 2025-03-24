baff411f3e28dfe5f474dde712f5d92054c0bf71
Farhan Islam <bsse1303@iit.du.ac.bd>
2025-03-24 14:48:29 +0600

    color change

--- Frontend/src/components/Timeline/Nodes/Method.jsx
+++ Frontend/src/components/Timeline/Nodes/Method.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Handle, Position } from '@xyflow/react';
 
-const Method = () => {
+const Method = ({ data }) => {
     return (
-        <div>
-            
+        <div className='min-w-44'>
+            <div className="p-4 border-black border-2 bg-[#C2EAAE] rounded-lg shadow-md cursor-pointer">
+                <Handle type="target" id="right" position={Position.Right} style={{ visibility: 'hidden' }} />
+                <Handle type="target" id="left" position={Position.Left} style={{ visibility: 'hidden' }} />
+                <Handle type="target" id="top" position={Position.Top} style={{ visibility: 'hidden' }} />
+                <Handle type="target" id="bottom" position={Position.Bottom} style={{ visibility: 'hidden' }} />
+                <div className="font-semibold text-black font-nunito">{data.label}</div>
+                <Handle type="source" id="right" position={Position.Right} style={{ visibility: 'hidden' }} />
+                <Handle type="source" id="left" position={Position.Left} style={{ visibility: 'hidden' }} />
+                <Handle type="source" id="top" position={Position.Top} style={{ visibility: 'hidden' }} />
+                <Handle type="source" id="bottom" position={Position.Bottom} style={{ visibility: 'hidden' }} />
+            </div>
         </div>
     );
 };
