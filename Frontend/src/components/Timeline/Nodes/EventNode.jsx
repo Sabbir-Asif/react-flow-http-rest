ffc48f65702f9b0f0941eaba717140207e243be6
Farhan Islam <bsse1303@iit.du.ac.bd>
2025-03-24 13:03:58 +0600

    Event Node added

--- Frontend/src/components/Timeline/Nodes/EventNode.jsx
+++ Frontend/src/components/Timeline/Nodes/EventNode.jsx
@@ -3,25 +3,21 @@ import { Handle, Position } from '@xyflow/react';
 
 const EventNode = ({ data }) => {
     return (
-        <div className='relative min-w-36'>
-            <div className='absolute -top-1 left-0 right-0 h-1 bg-gradient-to-r from-blue-500 to-green-500 rounded-t-lg'></div>
-            <div className="p-2 border-gray-300 rounded-md shadow-md cursor-pointer">
-                {/* <Handle type="target" position={Position.Left} style={{ visibility: 'hidden' }} /> */}
+        // <div className='relative  flex items-center justify-center'>
+        <div>
+            {/* Main node */}
+            <div className="w-24 h-24 shadow-md rounded-full bg-gradient-to-b from-gray-300 to-gray-100 flex flex-col items-center justify-center p-4 cursor-pointer border border-gray-400 relative">
                 <Handle type="target" id="right" position={Position.Right} style={{ visibility: 'hidden' }} />
                 <Handle type="target" id="left" position={Position.Left} style={{ visibility: 'hidden' }} />
                 <Handle type="target" id="top" position={Position.Top} style={{ visibility: 'hidden' }} />
                 <Handle type="target" id="bottom" position={Position.Bottom} style={{ visibility: 'hidden' }} />
-                <div className="font-semibold text-md font-merienda text-gray-800">{data.label}</div>
-                <div className='flex justify-between'>
-                    {
-                        data.year && (
-                            <div className="font-mono font-semibold text-blue-500">{data.year}</div>
-                        )
-                    }
-                    <span>
-                        event node
-                    </span>
-                </div>
+                
+                {/* Content */}
+                <div className="font-semibold text-md text-gray-800 text-center">{data.label}</div>
+                {data.year && (
+                    <div className="font-mono text-sm text-gray-600 mt-1">{data.year}</div>
+                )}
+                
                 <Handle type="source" id="right" position={Position.Right} style={{ visibility: 'hidden' }} />
                 <Handle type="source" id="left" position={Position.Left} style={{ visibility: 'hidden' }} />
                 <Handle type="source" id="top" position={Position.Top} style={{ visibility: 'hidden' }} />
