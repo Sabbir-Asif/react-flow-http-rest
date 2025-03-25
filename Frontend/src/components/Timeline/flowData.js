762e848693d1756a8962532469003078cbf6a523
Abir-Ashab <bsse1315@iit.du.ac.bd>
2025-03-25 16:53:28 +0600

    cookies are added

--- Frontend/src/components/Timeline/flowData.js
+++ Frontend/src/components/Timeline/flowData.js
@@ -86,8 +86,9 @@ export const initialNodes = [
     data: { 
       label: 'Netscape Introduced Cookies',
       year: '1990', 
+      drawer: true,
       description: 'This is the first node',
-      filePath: '/content/HTTP/0.9.md', 
+      filePath: '/data/cookies/1.2_intro.md', 
     },
   },
   {
